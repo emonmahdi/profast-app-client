3500adbdb0acab030791ffc4d57d565387f11bd2
emonmahdi <developeremon1997@gmail.com>
2025-11-05 12:06:28 +0600

    ready for deploy

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,8 +3,8 @@ import axios from "axios"
  
 
 const axiosInstance = axios.create({
-    baseURL: `http://localhost:5000`
-})
+  baseURL: `https://profast-app-server.vercel.app`,
+});
 
 const useAxios = () => {
   return  axiosInstance
